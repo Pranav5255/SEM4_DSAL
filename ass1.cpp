793ee795d649f6fd52950d6465edbf22c201d6fe
Pranav5255 <76805382+Pranav5255@users.noreply.github.com>
2025-04-21 12:34:29 +0530

    Update ass1.cpp

--- ass1.cpp
+++ ass1.cpp
@@ -1,222 +1,195 @@
-#include <iostream>
-#include <stack>
+#include<iostream>
+#include<stack>
 using namespace std;
 
 class Node {
     int data;
     Node *left, *right;
-
+    
     Node() {
-        data =0;
-        left = right = nullptr;
+        data=0;
+        left=right=nullptr;
     }
-
+    
     Node(int d) {
-        data =d;
-        left = right = nullptr;
+        data=d;
+        left=right=nullptr;
     }
-
+    
     friend class BST;
 };
 
-class BST {
-    Node *root = nullptr;
-
-    int height(Node *node) {
-        if(node == nullptr)
+class BST{
+    Node *root=nullptr;
+    
+    int height(Node *node){
+        if(node==nullptr)
             return 0;
-
+        
         int l = height(node->left);
         int r = height(node->right);
-
-        return max(l,r) + 1;
+        return max(l,r)+1;
     }
-
-    void swapAllNodes(Node *node) {
-        if(node == nullptr)
+    
+    void swapNodes(Node *node){
+        if(node==nullptr)
             return;
-
-        if(node->left or node->right) {
+        
+        if(node->left or node->right){
             Node *temp = node->left;
-            node->left = node->right;
-            node->right = temp;
+            node->left=node->right;
+            node->right=temp;
         }
-
-        swapAllNodes(node->right);
-        swapAllNodes(node->left);
+        
+        swapNodes(node->right);
+        swapNodes(node->left);
     }
-
-    void ipreorder() {
-        Node *curr = root;
+    
+    void preorder() {
+        Node *curr=root;
         stack<Node*> Stack;
-
-        while(!Stack.empty() or curr != nullptr) {
-            while(curr != nullptr) {
+        
+        while(!Stack.empty() or curr != nullptr){
+            while(curr!=nullptr){
                 Stack.push(curr);
-                curr = curr->left;
+                curr=curr->left;
             }
-            curr = Stack.top();
+            curr=Stack.top();
             Stack.pop();
             cout<<curr->data<<" ";
-            curr = curr->right;
+            curr=curr->right;
         }
     }
-
-    Node* minValueNode (Node *node) {
-        Node *curr = node;
-        while(curr->left != nullptr)
-            curr = curr->left;
-
+    
+    Node* minValueNode(Node* node){
+        Node *curr=node;
+        while(curr->left!=nullptr)
+            curr=curr->left;
+        
         return curr;
     }
-
-    Node* deleteSubTree(Node *root, int key) {
-
+    
+    Node* deletesubTree(Node *root, int key){
         if(key > root->data)
-            root->right = deleteSubTree(root->right, key);
-
+            root->right=deletesubTree(root->right, key);
         else if(key < root->data)
-            root->left = deleteSubTree(root->left, key);
-
-        else {
-            if(root->right == nullptr or root->left == nullptr) {
-                Node *temp = root->left ? root->left : root->right;
-
-                if(temp == nullptr) {
-                    temp = root;
-                    root = nullptr;
+            root->left=deletesubTree(root->left, key);
+        else{
+            if(root->right==nullptr or root->left==nullptr){
+                Node *temp=root->left ? root->left:root->right;
+                
+                if(temp==nullptr){
+                    temp=root;
+                    root=nullptr;
                 }
-
-                else {
-                    root->data = temp->data;
-                    root->left = root->right = nullptr;
+                
+                else{
+                    root->data=temp->data;
+                    root->left=root->right=nullptr;
                 }
-
+                
                 delete temp;
             }
-
-            else {
-                Node *temp = minValueNode(root->right);
-                root->data = temp->data;
-
-                root->right = deleteSubTree(root->right, temp->data);
+            
+            else{
+                Node *temp=minValueNode(root->right);
+                root->data=temp->data;
+                
+                root->right=deletesubTree(root->right, temp->data);
             }
         }
         
         return root;
     }
-
-    public: 
-
-    void insert(int d) {
-        Node *curr = root, *prev = nullptr;
-
-        if(curr == nullptr) {
-            root = new Node(d);
+    
+    public:
+    void insert(int d){
+        Node *curr=root, *prev =nullptr;
+        
+        if(curr==nullptr){
+            root=new Node(d);
         }
-
-        else {
-            while(curr != nullptr) {
-                if(d > curr->data) {
-                    prev = curr;
-                    curr = curr->right;
+        
+        else{
+            while(curr!=nullptr){
+                if(d>curr->data){
+                    prev=curr;
+                    curr=curr->right;
                 }
-
-                else if( d < curr->data ) {
-                    prev = curr;
-                    curr = curr->left;
+                else if(d<curr->data){
+                    prev=curr;
+                    curr=curr->left;
                 }
-
-                else {
-                    cout<<"Node already exists."<<endl;
+                else{
+                    cout<<"Node already exists"<<endl;
                     return;
                 }
             }
             
-            if( d > prev->data ) {
-                Node *temp = new Node(d);
-                prev->right = temp;
+            if(d>prev->data){
+                Node *temp=new Node(d);
+                prev->right=temp;
             }
-
-            else {
-                Node *temp = new Node(d);
-                prev->left = temp;
+            
+            else{
+                Node *temp=new Node(d);
+                prev->left=temp;
             }
         }
     }
-
-    void search(int d) {
-        Node *curr = root;
-        while(curr != nullptr) {
-            if(d > curr->data)
-                curr = curr->right;
-            else if(d < curr->data)
-                curr = curr->left;
-            else {
+    
+    void search(int d){
+        Node *curr=root;
+        while(curr!=nullptr){
+            if(d>curr->data)
+                curr=curr->right;
+            else if(d<curr->data)
+                curr=curr->left;
+            else{
                 cout<<"\nNode found"<<endl;
                 return;
             }
         }
-        cout<<"\nNode does not exist"<<endl;
+        cout<<"\nNode doesn't exist"<<endl;
     }
-
     void del(int key){
-        root = deleteSubTree(root, key);
+        root=deletesubTree(root, key);
     }
-
-    void getHeight() {
-        cout<<"\nNumber of nodes in the longest path from root are: "<<height(root) + 1<<endl;
+    
+    void getheight(){
+        cout<<"\nNumber of nodes in the longest path: "<<height(root)+1<<endl;
     }
-
-    void getMinValue() {
+    
+    void getminValue(){
         cout<<"\nMinimum value in the tree: "<<minValueNode(root)->data<<endl;
     }
-
-    void getAscending() {
+    void getAscending(){
         cout<<"\nValues in ascending order are: ";
-        ipreorder();
+        preorder();
     }
-
-    void printSwapped() {
-        swapAllNodes(root);
-        cout<<"\nTree when swapped: ";
-        ipreorder();
+    void printSwapped(){
+        swapNodes(root);
+        cout<<"\nTrees are swapped:"<<endl;
+        preorder();
     }
 };
 
-int main() {
+int main(){
     BST tree;
-
+    
     tree.insert(50);
-    tree.insert(70);
-    tree.insert(60);
-    tree.insert(20);
-    tree.insert(90);
-    tree.insert(10);
-    tree.insert(40);
+    tree.insert(51);
+    tree.insert(68);
+    tree.insert(37);
     tree.insert(100);
-
+    tree.insert(29);
+    
     tree.getAscending();
-
-    tree.del(10);
-
+    
+    tree.del(37);
+    
     tree.getAscending();
-    tree.getHeight();
-
-    BST tree2;
-
-    tree2.insert(10);
-    tree2.insert(12);
-    tree2.insert(5);
-    tree2.insert(4);
-    tree2.insert(20);
-    tree2.insert(8);
-    tree2.insert(7);
-    tree2.insert(15);
-    tree2.insert(13);
-
-    tree2.getAscending();
-    tree2.getHeight();
+    tree.getheight();
     
     return 0;
 }
